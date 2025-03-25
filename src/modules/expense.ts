eae45776777bc00d7c2b1ca01293b6dc62c55424
MYCS <MYCS.@gmail.com>
2025-03-25 16:16:50 +0200

    my Date class

--- src/modules/expense.ts
+++ src/modules/expense.ts
@@ -1,12 +1,16 @@
+import { myDate } from "./myDate";
+
 export class Expence {
     provider: String;
     category: String;
     sum: number;
-    date: number;
-    constructor({ provider, category, sum, date }: { provider: String, category: String, sum: number, date: number }) {
+    date: myDate;
+    constructor({ provider, category, sum, date }: { provider: String, category: String, sum: number, date: myDate }) {
         this.provider = provider;
         this.category = category;
         this.sum = sum;
         this.date = date;
     }
 }
+
+
